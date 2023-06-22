332679dc1b85288586506d7262fdfe3de347d3f6
Rudolf Olah <rudolf.olah.to@gmail.com>
2023-06-21 23:36:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GitHub Traffic Check
+Prints out all traffic insights for GitHub repositories that you have access to
+
+[Licensed under MIT license](./LICENSE)
+
+## Usage
+
+1. [Create a personal web access token](https://github.com/settings/tokens/new), with `repo` scope
+2. Copy [`.env.sample`](./.env.sample) to `.env` and fill in the details
+3. `npm install`
+4. `npm start`
---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { Octokit } = require("@octokit/rest");
+
+async function main() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
+  const reposResponse = await octokit.repos.listForAuthenticatedUser();
+  console.log('repo_name,stars,views,uniques');
+  for (let repo of reposResponse.data) {
+    if (repo.private) {
+      continue;
+    }
+    const trafficResponse = await octokit.repos.getViews({
+      owner: repo.owner.login,
+      repo: repo.name
+    });
+    console.log(`${repo.full_name},${repo.stargazers_count},${trafficResponse.data.count},${trafficResponse.data.uniques}`)
+  }
+}
+
+main();
